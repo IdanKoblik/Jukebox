ab180e6bf9724887f4f908296ec69efaf8e6300c
idank <idankob@gmail.com>
2024-10-27 15:05:59 +0200

    Update workflow

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # CHANGELOG
 
-## 15.0.0-t
+## 16.0.0-t
 - [cicd] testing version for cicd
 
 ## 0.0.3
--- build-scripts/prepare-web-output.sh
+++ build-scripts/prepare-web-output.sh
@@ -3,6 +3,9 @@
 if [ ! -d $docs ]; then
   mkdir $docs
   unzip artifacts/webHelpHI2-all.zip -d $docs
+
+  git rm -rf artifacts
+
   mkdir -p $docs/shared
   mkdir -p $docs/paper
   mkdir -p $docs/adventure
@@ -11,4 +14,12 @@ if [ ! -d $docs ]; then
   cp -a shared/build/docs/javadoc/* $docs/shared
   cp -a paper/build/docs/javadoc/* $docs/paper
   cp -a adventure/build/docs/javadoc/* $docs/adventure
+
+  git rm -rf shared
+  git rm -rf paper
+  git rm -rf adventure
+
+  git add $docs/
+
+  git commit -m "Output ready"
 fi
